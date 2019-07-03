ede26954bfc3f39241a63addb2f54b9838a5b7e2
chenli <chenli@letto8.com>
2019-07-03 09:58:24 +0800

    全埋点测试

--- app/src/main/java/com/cl/tracker/app/MyApp.java
+++ app/src/main/java/com/cl/tracker/app/MyApp.java
@@ -23,7 +23,7 @@ public class MyApp extends Application {
                 // 开启log
                 .openLog(true)
                 // 设置日志的上传策略
-                .setUploadCategory(UPLOAD_CATEGORY.REAL_TIME.getValue())
+                .setUploadCategory(UPLOAD_CATEGORY.TIME_MINUTER.getValue())
                 // 设置埋点信息上传的URL
                 .setServerUrl(SA_SERVER_URL)
                 //本地缓存的最大事件数目，当累积日志量达到阈值时发送数据，默认值 10
@@ -31,7 +31,7 @@ public class MyApp extends Application {
                 //设置本地缓存最多事件条数，默认为 10000 条
                 .setMaxCacheSize(10000)
                 //设置多少分钟上传一次默认5分钟最大60分钟
-                .setMinutes(20)
+                .setMinutes(1)
                 .initializeDb(this);
 
         Tracker.getInstance().init(this, configuration);
